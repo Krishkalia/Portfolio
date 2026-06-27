bec207898c7e3032b4ea1370b358e8cba3f56c0c
Krish Kalia <Krishkaliajal10@gmail.com>
2026-06-27 21:07:01 +0530

    feat: integrate intro audio playback on site visit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react";
+import { lazy, Suspense, useEffect } from "react";
 import "./App.css";
 
 const CharacterModel = lazy(() => import("./components/Character"));
@@ -6,6 +6,30 @@ const MainContainer = lazy(() => import("./components/MainContainer"));
 import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
+  useEffect(() => {
+    const audio = new Audio('/voice/intro.mp3');
+    const playAudio = () => {
+      audio.play().catch(e => console.log("Autoplay prevented:", e));
+      document.removeEventListener('click', playAudio);
+      document.removeEventListener('touchstart', playAudio);
+      document.removeEventListener('keydown', playAudio);
+    };
+
+    // Try autoplay first
+    audio.play().catch(e => {
+      // If prevented, attach listeners
+      document.addEventListener('click', playAudio);
+      document.addEventListener('touchstart', playAudio);
+      document.addEventListener('keydown', playAudio);
+    });
+
+    return () => {
+      document.removeEventListener('click', playAudio);
+      document.removeEventListener('touchstart', playAudio);
+      document.removeEventListener('keydown', playAudio);
+    };
+  }, []);
+
   return (
     <>
       <LoadingProvider>
