cff6b9aaa49fe7168edc749851bb602e80c4c27c
Krish Kalia <Krishkaliajal10@gmail.com>
2026-06-27 20:45:25 +0530

    perf: disable ScrollSmoother on mobile to improve low-end device performance

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,18 +10,20 @@ export let smoother;
 
 const Navbar = () => {
   useEffect(() => {
-    smoother = ScrollSmoother.create({
-      wrapper: "#smooth-wrapper",
-      content: "#smooth-content",
-      smooth: 1.7,
-      speed: 1.7,
-      effects: true,
-      autoResize: true,
-      ignoreMobileResize: true,
-    });
+    if (window.innerWidth > 1024) {
+      smoother = ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 1.7,
+        speed: 1.7,
+        effects: true,
+        autoResize: true,
+        ignoreMobileResize: true,
+      });
 
-    smoother.scrollTop(0);
-    smoother.paused(true);
+      smoother?.scrollTop(0);
+      smoother?.paused(true);
+    }
 
     let links = document.querySelectorAll(".header ul a");
     links.forEach((elem) => {
@@ -31,12 +33,14 @@ const Navbar = () => {
           e.preventDefault();
           let elem = e.currentTarget;
           let section = elem.getAttribute("data-href");
-          smoother.scrollTo(section, true, "top top");
+          smoother?.scrollTo(section, true, "top top");
         }
       });
     });
     window.addEventListener("resize", () => {
-      ScrollSmoother.refresh(true);
+      if (window.innerWidth > 1024) {
+        ScrollSmoother.refresh(true);
+      }
     });
   }, []);
   return (
